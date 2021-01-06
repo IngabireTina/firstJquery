9d67fdfa6b101e19e6352019410b4bc939a5a38f
IngabireTina <ingabire067@gmail.com>
2021-01-06 06:41:15 +0200

    first commit in jquery

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+// jQuery('h1').click(function(){
+//     alert("This is a heading tag")
+//   });
+
+//   jQuery('p').click(function(){
+//     alert("This is a paragraph")
+//   });
+
+//   jQuery('img').click(function(){
+//     alert("This is an image")
+//   });
+
+
+var languages = ['HTML', 'CSS', 'Javascript'];
+for( var i= 0; i <languages.length; i++){
+    console.log("i love" + languages[i]);
+}
